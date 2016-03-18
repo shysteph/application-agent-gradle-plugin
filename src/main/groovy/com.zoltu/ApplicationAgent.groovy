4490ffdff49102cdb0c9881ec966ad14ae3ab2b7
Micah Zoltu <micah@zoltu.net>
2016-03-17 23:06:13 -0700

    RepoCreator created repository from template . The template can be found at https://github.com/Zoltu/Templates.GradlePlugin.Groovy/

---
+++ src/main/groovy/com.zoltu/ApplicationAgent.groovy
@@ -0,0 +1,10 @@
+package com.zoltu
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class ApplicationAgentPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		// TODO: write plugin code here
+	}
+}
